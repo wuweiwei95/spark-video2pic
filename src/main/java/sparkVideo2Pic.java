326401a72463217cc084b69a2340f086298b878a
rango905 <wuweiwei.chn@gmail.com>
2017-05-16 14:05:05 +0800

    test foreach function

--- src/main/java/sparkVideo2Pic.java
+++ src/main/java/sparkVideo2Pic.java
@@ -6,12 +6,14 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.spark.SparkConf;
 
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.RectVector;
 import org.bytedeco.javacpp.opencv_core.Scalar;
@@ -24,35 +26,41 @@ import javax.imageio.ImageIO;
 
 public class sparkVideo2Pic {
 
-    public static void video2Pic(File inputFile, String outputPath) throws IOException {
-        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
+    public static void video2Pic(String inputFile, String outputPath) throws IOException {
 
-        try {
-            grabber.start();
-        } catch (Exception e) {
-            // TODO exception print
-            e.printStackTrace();
-        }
-
-        long frameLength = grabber.getLengthInFrames();
-
-        // To grab frames per 0.5 seconds
-        for (int i=1; i < frameLength; i += grabber.getFrameRate()/2) {
-            Java2DFrameConverter converter = new Java2DFrameConverter();
-            grabber.setFrameNumber(i);
-
-            BufferedImage bi = converter.getBufferedImage(grabber.grab());
-            ImageIO.write(bi, "jpg", new File(outputPath + "frame" + i + ".jpg"));
-        }
-
-        // To know number for each video
-        System.out.println("Video " + inputFile + " have " + frameLength + " frames.");
-
-        try {
-            grabber.stop();
-        } catch (FrameGrabber.Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("filename is " + inputFile);
+        System.out.println("output file path is " + outputPath);
+        return;
+//        // TODO need to load file in local memory FS, or find a memory Grabber API
+//        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
+//
+//        try {
+//            grabber.start();
+//        } catch (Exception e) {
+//            // TODO exception print
+//            e.printStackTrace();
+//        }
+//
+//        long frameLength = grabber.getLengthInFrames();
+//
+//        // To grab frames per 0.5 seconds
+//        for (int i=1; i < frameLength; i += grabber.getFrameRate()/2) {
+//            Java2DFrameConverter converter = new Java2DFrameConverter();
+//            grabber.setFrameNumber(i);
+//
+//            BufferedImage bi = converter.getBufferedImage(grabber.grab());
+//            // TODO write to HDFS
+//            ImageIO.write(bi, "jpg", new File(outputPath + "frame" + i + ".jpg"));
+//        }
+//
+//        // To know number for each video
+//        System.out.println("Video " + inputFile + " have " + frameLength + " frames.");
+//
+//        try {
+//            grabber.stop();
+//        } catch (FrameGrabber.Exception e) {
+//            e.printStackTrace();
+//        }
 
 
     }
@@ -60,15 +68,19 @@ public class sparkVideo2Pic {
 
         SparkConf conf = new SparkConf().setAppName("Video split");
         JavaSparkContext jsc = new JavaSparkContext(conf);
-        String outputPath = "hdfs://master:9000/pic-out/";
+        String outputPath = "hdfs://master:9000/output/";
 
         // convert input path into RDD, wholeTextFiles will read all the video file into memory
-        JavaPairRDD<String, String> videosContentsRDD = jsc.wholeTextFiles("hdfs://master:9000/video-input/");
+        JavaRDD<String> videoNamesRDD = jsc.textFile("hdfs://master:9000/videos/");
         long startTime = System.currentTimeMillis();
-        System.out.println("Process files number is  " + videosContentsRDD.count());
+        System.out.println("Process files number is  " + videoNamesRDD.count());
 
         // distribute video process
-
+        videoNamesRDD.foreach(new VoidFunction<String>() {
+            public void call (String videoFile) throws IOException {
+                video2Pic(videoFile, outputPath);
+            }
+        });
         // get the process time
         long endTime   = System.currentTimeMillis();
         long totalTime = endTime - startTime;
