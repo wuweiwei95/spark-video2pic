dd273793cbd32e11d02f9ee43cb9c011c7ae22c5
rango905 <wuweiwei.chn@gmail.com>
2017-05-23 16:31:36 +0800

    basic video decode
    1. copy HDFS video file to worker local filesystem
    2. decode video file to image file in worker local filesystem
    3. copy image file to HDFS out-pics path

--- src/main/java/sparkVideo2Pic.java
+++ src/main/java/sparkVideo2Pic.java
@@ -2,8 +2,10 @@
  * Created by wuweiwei on 05/05/2017.
  */
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +18,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.VoidFunction;
+import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.RectVector;
 import org.bytedeco.javacpp.opencv_core.Scalar;
@@ -30,44 +33,66 @@ import static java.lang.Thread.sleep;
 
 public class sparkVideo2Pic {
 
-    public static void video2Pic(String inputFile, String outputPath) throws IOException {
+    public static void copyFile(String from, String to) throws IOException {
+        Configuration conf = new Configuration();
+        Path fromPath = new Path(from);
+        Path toPath = new Path(to);
 
-        System.out.println("filename is " + inputFile);
-        System.out.println("output file path is " + outputPath);
-        return;
-//        // TODO need to load file in local memory FS, or find a memory Grabber API
-//        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
-//
-//        try {
-//            grabber.start();
-//        } catch (Exception e) {
-//            // TODO exception print
-//            e.printStackTrace();
-//        }
-//
-//        long frameLength = grabber.getLengthInFrames();
-//
-//        // To grab frames per 0.5 seconds
-//        for (int i=1; i < frameLength; i += grabber.getFrameRate()/2) {
-//            Java2DFrameConverter converter = new Java2DFrameConverter();
-//            grabber.setFrameNumber(i);
-//
-//            BufferedImage bi = converter.getBufferedImage(grabber.grab());
-//            // TODO write to HDFS
-//            ImageIO.write(bi, "jpg", new File(outputPath + "frame" + i + ".jpg"));
-//        }
-//
-//        // To know number for each video
-//        System.out.println("Video " + inputFile + " have " + frameLength + " frames.");
-//
-//        try {
-//            grabber.stop();
-//        } catch (FrameGrabber.Exception e) {
-//            e.printStackTrace();
-//        }
+        FSDataInputStream is = fromPath.getFileSystem(conf).open(fromPath);
+        FSDataOutputStream os = toPath.getFileSystem(conf).create(toPath);
 
+        IOUtils.copyBytes(is, os, conf);
 
+        is.close();
+        os.close();
     }
+
+    public static void video2Pic(String inputFilePath, String outputPath) throws IOException {
+        System.out.println("filename is " + inputFilePath);
+        System.out.println("output file path is " + outputPath);
+
+        String inputFileName = inputFilePath.substring(inputFilePath.lastIndexOf("/") + 1);
+        String extension = inputFileName.substring(inputFileName.lastIndexOf("."));
+        String inputName = inputFileName.substring(0, inputFileName.lastIndexOf("."));
+        String tmpPicFile;
+
+        File localIn = File.createTempFile(inputName + "-", extension, new File("/tmp/"));
+
+        // copy video file from HDFS to worker tmp path
+        copyFile(inputFilePath, "file://" + localIn.getPath());
+
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(localIn.getPath());
+
+        try {
+            grabber.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        long frameLength = grabber.getLengthInFrames();
+
+        // To grab frames per 0.5 seconds
+        for (int i = 1; i < frameLength; i += grabber.getFrameRate() / 2) {
+            Java2DFrameConverter converter = new Java2DFrameConverter();
+            grabber.setFrameNumber(i);
+            tmpPicFile = "/tmp/" + inputName + "-" + "frame" + i + ".jpg";
+            BufferedImage bi = converter.getBufferedImage(grabber.grab());
+            // TODO write to HDFS
+            ImageIO.write(bi, "jpg", new File(tmpPicFile));
+
+            copyFile("file://" + tmpPicFile, outputPath + inputName + "-" + "frame" + i + ".jpg");
+        }
+
+        // To know number for each video
+        System.out.println("Video " + inputFilePath + " have " + frameLength + " frames.");
+
+        try {
+            grabber.stop();
+        } catch (FrameGrabber.Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
 
         if (args.length < 2) {
